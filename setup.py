2921a5da2430f1f973245c978c1aab4f6e89029c
Amanda Butler <90015455+amanda3utler@users.noreply.github.com>
2021-11-19 06:15:34 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setuptools.setup(
+    name="ambify",
+    version="0.1",
+    author="Amanda Butler",
+    author_email="amanda.butler@yale.edu",
+    description='An all-in-one ambient music player',
+    packages=["ambify"],
+    package_data={'ambify': ['outputs/sounds/*.mp3']}
+    install_requires=[
+        'numpy>=1.17',
+        'scipy',
+        'spotipy',
+        'matplotlib>=3',
+        'streamlit',
+        'pygame',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Spotify Users",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
